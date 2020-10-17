16f10540ee26fed11c23b200a9efe29d7ade765f
FakhryanZ <fakhryanmuhammad@gmail.com>
2020-10-17 09:23:45 +0700

    add migration

---
+++ database/migrations/2020_10_17_011532_create_skor_awal_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkorAwalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skor_awal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('alternatif_id');
+            $table->unsignedBigInteger('kriteria_id');
+            $table->foreign('alternatif_id')->references('id')->on('alternatif');
+            $table->foreign('kriteria_id')->references('id')->on('kriteria');
+            $table->integer('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skor_awal');
+    }
+}
