98a748d1f34c18c5172d0515b550b6ac89d8aa5d
FakhryanZ <fakhryanmuhammad@gmail.com>
2020-10-24 10:15:25 +0700

    edit topsis controller

--- resources/views/topsis.blade.php
+++ resources/views/topsis.blade.php
@@ -63,4 +63,39 @@
                 </tbody>
             </table>
   </div>
+
+  <div class="card card-body mt-4">
+    <h4>Normalisasi</h4>
+            <table class="table table-striped table-bordered table-hover">
+                <thead class="text-center">
+                    <tr>
+                      <th rowspan='2'>No.</th>
+                      <th rowspan='2'>Kode</th>
+                      <th rowspan='2'>Alternatif</th>
+                      <th colspan='{{$kriteria->count()}}' class="text-center">Kriteria</th>
+                    </tr>
+                    <tr>
+                      @foreach($kriteria as $k)
+                      <th>{{$k->nama_kriteria}}</th>
+                      @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                <div hidden>{{$index=0}}</div>
+                @foreach($data as $nama=>$krit)
+                <tr>
+                  <td align='center'>{{++$index}}.</td>
+                  <td align='center'>A{{$index}}</td>
+                  <td>{{$nama}}</td>
+                  
+                  {{-- {{dd($arrnormalterbobot)}} --}}
+                  {{-- @foreach($arrnormalterbobot as $k)
+                  {{dd($k)}}
+                  <td align='center'>{{$k}}</td>
+                  @endforeach --}}
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+  </div>
 @endsection
